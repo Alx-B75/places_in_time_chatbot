4d45b79d680e51a17c8475e0bd99ccf7b87def9d
Alex Bunting <alexander_bunting@hotmail.com>
2025-06-25 21:12:51 +0200

    Debug script for render in main

--- entrypoint.py
+++ entrypoint.py
@@ -2,13 +2,9 @@
 Entrypoint to run the FastAPI app from the backend package.
 """
 
-import sys
-import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
+import os, sys
+
+sys.path.insert(0, os.path.dirname(__file__))
 
 from backend.main import app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("entrypoint:app", host="0.0.0.0", port=10000)
