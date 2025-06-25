fc56cd56d0f0c31a5cc89692ee0639d343040cbb
Alex Bunting <alexander_bunting@hotmail.com>
2025-06-25 20:32:54 +0200

    Main updated with entrypoint.py to prepare for render finally.

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,13 @@
 Entrypoint to run the FastAPI app from the backend package.
 """
 
-import uvicorn
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from backend.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=10000)
+    import uvicorn
+    uvicorn.run("entrypoint:app", host="0.0.0.0", port=10000)
