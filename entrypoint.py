5371c8cdf773755af34be7f7999cc8927ec38f85
Alex Bunting <alexander_bunting@hotmail.com>
2025-06-25 20:40:30 +0200

    Main updated with entrypoint.py to prepare for render finally.

--- entrypoint.py
+++ entrypoint.py
@@ -5,7 +5,7 @@ Entrypoint to run the FastAPI app from the backend package.
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 
 from backend.main import app
 
